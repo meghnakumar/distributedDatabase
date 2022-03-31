e6b2654ba0b3564f56e9b45ffdb950ce7239239f
shathish annamalai <shatishsyn@gmail.com>
2022-03-31 13:25:41 -0300

    added homepage get api changes

--- src/main/java/com/csci5408/distributeddatabase/DistributedDatabaseApplication.java
+++ src/main/java/com/csci5408/distributeddatabase/DistributedDatabaseApplication.java
@@ -1,6 +1,5 @@
 package com.csci5408.distributeddatabase;
 
-import com.csci5408.distributeddatabase.globalmetadatahandler.GlobalMetadataHandler;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
