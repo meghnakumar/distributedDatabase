0d8d49c8c3c9f2857a363261cc9f3a8fd8ca01fd
shathish annamalai <shatishsyn@gmail.com>
2022-03-31 12:56:55 -0300

    global metadata flush changes

--- src/main/java/com/csci5408/distributeddatabase/DistributedDatabaseApplication.java
+++ src/main/java/com/csci5408/distributeddatabase/DistributedDatabaseApplication.java
@@ -3,7 +3,6 @@ package com.csci5408.distributeddatabase;
 import com.csci5408.distributeddatabase.globalmetadatahandler.GlobalMetadataHandler;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class DistributedDatabaseApplication
@@ -11,8 +10,5 @@ public class DistributedDatabaseApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DistributedDatabaseApplication.class, args);
-
-		GlobalMetadataHandler handler = new GlobalMetadataHandler();
-		handler.initGlobalMetaData();
 	}
 }
--- src/main/java/com/csci5408/distributeddatabase/globalmetadatahandler/GlobalMetadataHandler.java
+++ src/main/java/com/csci5408/distributeddatabase/globalmetadatahandler/GlobalMetadataHandler.java
@@ -1,6 +1,7 @@
 package com.csci5408.distributeddatabase.globalmetadatahandler;
 
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
@@ -13,12 +14,14 @@ public class GlobalMetadataHandler
         try
         {
             OutputStream output = new FileOutputStream("src/main/resources/GlobalMetadata.properties");
-            Properties prop = new Properties();
+            Properties prop = getGlobalMetadataProperties();
 
-            prop.setProperty("currentinstance", "localhost");
-            prop.store(output, "Global Metadata File");
+            if(prop.getProperty("currentinstance")==null)
+            {
+                prop.setProperty("currentinstance", "localhost");
+                prop.store(output, "Global Metadata File");
+            }
 
-            System.out.println(prop);
             result=true;
         }
         catch (Exception e)
@@ -28,4 +31,45 @@ public class GlobalMetadataHandler
         return result;
     }
 
+    public boolean writeToMetaData(String propName, String propValue)
+    {
+        boolean result = false;
+        try
+        {
+            OutputStream output = new FileOutputStream("src/main/resources/GlobalMetadata.properties", true);
+            Properties  prop = getGlobalMetadataProperties();
+
+            if(prop.getProperty(propName)==null)
+            {
+                prop.setProperty(propName, propValue);
+            }
+            else
+            {
+                prop.replace(propName, propValue);
+            }
+
+            prop.store(output, null);
+            result=true;
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Properties getGlobalMetadataProperties()
+    {
+        Properties prop = new Properties();
+        try
+        {
+            InputStream input = GlobalMetadataHandler.class.getClassLoader().getResourceAsStream("GlobalMetadata.properties");
+            prop.load(input);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return prop;
+    }
 }
