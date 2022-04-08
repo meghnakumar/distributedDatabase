5092b3125868ea181d47f8396d44b61d3d7fee4d
shathish annamalai <shatishsyn@gmail.com>
2022-04-08 14:41:45 -0300

    fixed issues with property file operations

--- src/main/java/com/csci5408/distributeddatabase/globalmetadatahandler/GlobalMetadataConstants.java
+++ src/main/java/com/csci5408/distributeddatabase/globalmetadatahandler/GlobalMetadataConstants.java
@@ -2,9 +2,10 @@ package com.csci5408.distributeddatabase.globalmetadatahandler;
 
 public class GlobalMetadataConstants
 {
-    public static final String GLOBAL_METADATA_FILE = "GlobalMetadata.properties";
+    public static final String GLOBAL_METADATA_FILE_PATH = "GlobalMetadata.properties";
 
     public static final String INSTANCE_CURRENT = "localhost";
 
     public static final String INSTANCE_OTHER = "other";
 }
+
