c3f07876264cc5c3b261187ffeb5a0124b0ef2c9
shathish annamalai <shatishsyn@gmail.com>
2022-04-08 14:49:17 -0300

    route API working

--- src/main/java/com/csci5408/distributeddatabase/controller/DatabaseController.java
+++ src/main/java/com/csci5408/distributeddatabase/controller/DatabaseController.java
@@ -1,8 +1,18 @@
 package com.csci5408.distributeddatabase.controller;
 
+import com.csci5408.distributeddatabase.globalmetadatahandler.GlobalMetadataHandler;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.AsyncRestOperations;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Properties;
 
 @RestController
 public class DatabaseController
@@ -19,5 +29,49 @@ public class DatabaseController
     {
         System.err.println("index api called");
         return "Index API return";
+    }
+
+    @GetMapping("/test")
+    public String redirectTest()
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "http://localhost:8080/home";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        return response.getBody();
+    }
+
+    @GetMapping("/getGlobalMetaDataProp")
+    public Properties getGlobalMetaData()
+    {
+        GlobalMetadataHandler globalMetadataHandler = new GlobalMetadataHandler();
+        return globalMetadataHandler.getGlobalMetadataProperties();
+    }
+
+    @PostMapping("/redirectUpdateGlobalMetaDataProp")
+    public Properties redirectUpdateGlobalMetadataProp(@RequestParam  String propName, @RequestParam  String propValue)
+    {
+        System.err.println("received value= "+propName+" _ "+propValue);
+        GlobalMetadataHandler globalMetadataHandler = new GlobalMetadataHandler();
+        globalMetadataHandler.writeToMetaData(propName, propValue);
+        return globalMetadataHandler.getGlobalMetadataProperties();
+    }
+
+    @PostMapping("/updateGlobalMetaDataProp")
+    public String updateGlobalMetadataProp(@RequestParam  String propName, @RequestParam  String propValue)
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:8080/redirectUpdateGlobalMetaDataProp";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
+        map.add("propName", propName);
+        map.add("propValue", propValue);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity( url, request , String.class );
+
+        return response.getBody();
     }
 }
