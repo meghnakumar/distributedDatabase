a1bce49d0e54a900c1561119603bdb3a504e7d92
shathish annamalai <shatishsyn@gmail.com>
2022-04-08 12:32:48 -0300

    fixed issues with API

--- src/main/java/com/csci5408/distributeddatabase/controller/DatabaseController.java
+++ src/main/java/com/csci5408/distributeddatabase/controller/DatabaseController.java
@@ -2,8 +2,9 @@ package com.csci5408.distributeddatabase.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class DatabaseController
 {
     @GetMapping("/")
@@ -11,5 +12,12 @@ public class DatabaseController
     {
         System.err.println("get api called");
         return "Distributed databse application";
+    }
+
+    @GetMapping("/home")
+    public String indexPage()
+    {
+        System.err.println("index api called");
+        return "Index API return";
     }
 }
