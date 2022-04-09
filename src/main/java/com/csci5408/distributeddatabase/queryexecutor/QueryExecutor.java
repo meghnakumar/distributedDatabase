d4d194c8876438b72c9b9fef9658966f057b5576
shathish annamalai <shatishsyn@gmail.com>
2022-04-09 12:10:59 -0300

    distributed changes

--- src/main/java/com/csci5408/distributeddatabase/queryexecutor/QueryExecutor.java
+++ src/main/java/com/csci5408/distributeddatabase/queryexecutor/QueryExecutor.java
@@ -1,11 +1,14 @@
 package com.csci5408.distributeddatabase.queryexecutor;
 
 import com.csci5408.distributeddatabase.query.CreatDatabaseQuery;
+import com.csci5408.distributeddatabase.query.CreateTableQuery;
 import com.csci5408.distributeddatabase.query.Query;
 import com.csci5408.distributeddatabase.query.parsers.Parser;
 import com.csci5408.distributeddatabase.query.parsers.QueryParser;
+import com.csci5408.distributeddatabase.queryexecutor.util.QueryExecutorUtil;
 
 import static com.csci5408.distributeddatabase.query.QueryType.CREATE_DATABASE;
+import static com.csci5408.distributeddatabase.query.QueryType.CREATE_TABLE;
 
 public class QueryExecutor
 {
@@ -19,7 +22,6 @@ public class QueryExecutor
     public String executeQuery()
     {
         //TODO handle query execution
-
         try
         {
             QueryParser parser = new QueryParser();
@@ -30,6 +32,19 @@ public class QueryExecutor
                 CreatDatabaseQuery creatDatabaseQuery = (CreatDatabaseQuery) query;
                 CreateDatabaseExecutor createDatabaseExecutor = new CreateDatabaseExecutor(creatDatabaseQuery);
                 createDatabaseExecutor.execute();
+            }
+            else if(query.getQueryType().equals(CREATE_TABLE))
+            {
+                if(!QueryExecutorUtil.isDatabaseChosen())
+                {
+                    return "No Database has been chosen please choose a database";
+                }
+                else
+                {
+                    CreateTableQuery createTableQuery = (CreateTableQuery) query;
+                    CreateTableExecutor createTableExecutor = new CreateTableExecutor(createTableQuery, QueryExecutorUtil.getChosenDatabase());
+                    createTableExecutor.execute();
+                }
             }
         }
         catch (Exception ex)
